3dbafb0c467ddab89296ed087e9c70bec4239c9c
clai <126102086+LaiCharlie@users.noreply.github.com>
2024-01-04 16:39:31 +0800

    Add files via upload

---
+++ lab2/INP112 Lab02 - CodiMD_files/mathjax-config-extra.js
@@ -0,0 +1,11 @@
+window.MathJax = {
+  messageStyle: 'none',
+  skipStartupTypeset: true,
+  tex2jax: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    processEscapes: true
+  },
+  TeX: {
+    extensions: ['mhchem.js']
+  }
+}
